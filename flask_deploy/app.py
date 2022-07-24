ca6ba9a48b9f7cf1188946492df58e569d6df4e3
Jeki <8jane7@gmail.com>
2022-07-24 12:32:24 +0300

    New changes 2.0

---
+++ flask_deploy/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def root_page():
+    return 'Hello World'
+
+
+@app.route('/content')
+def content_page():
+    return '<h1>Testing the flask app</h1>'
+
+
+@app.route('/about')
+def about_page():
+    return '<b>Эта страница, описывающая модель машинного обучения</b>'
+
+
+#@app.route('/index/<int:num>')
+#def index_page(num):
+#    return render_template('index.html', n=num)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """
+    For rendering results on HTML GUI
+    """
+    features = [int(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 1)
+
+    return render_template('index.html', prediction_text='Price is: {}'.format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
